58a812ccfe6106aa98a7ef8ae53ce0d16a04b973
Cordelia Cavill <159061167+cmc227@users.noreply.github.com>
2024-07-30 20:41:38 +0100

    Update model.py

--- decimer_segmentation/mrcnn/model.py
+++ decimer_segmentation/mrcnn/model.py
@@ -32,6 +32,7 @@ from distutils.version import LooseVersion
 os.environ["CUDA_VISIBLE_DEVICES"] = "4"
 
 gpus = tf.config.experimental.list_physical_devices("GPU")
+print(gpus)
 for gpu in gpus:
     tf.config.experimental.set_memory_growth(gpu, True)
 
